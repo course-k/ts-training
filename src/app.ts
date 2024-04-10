bd15becd57c10a8ea321eed85f3165282961eede
course-k <course.k.ky@gmail.com>
2024-04-11 04:20:32 +0900

    interface

--- src/app.ts
+++ src/app.ts
@@ -1,65 +1,28 @@
-class Animal {
-  protected favorite: string[] = [];
-  get latestAddress() {
-    return this.address;
-  }
-  set latestAddress(address: string) {
-    this.address = address;
-  }
-  constructor(
-    private readonly animalId: string,
-    public type: string,
-    public crying: string,
-    private address: string
-  ) {}
-  greeting(this: Animal) {
-    console.log(`${this.crying}! 私は${this.type}です。`);
-  }
-  addFavorite(this: Animal, fav: string) {
-    this.favorite.push(fav);
-  }
-  printFav(this: Animal) {
-    console.log(this.favorite);
-  }
+interface Animal {
+  name: string;
+  crying(cry: string): void;
 }
 
-abstract class Person {
-  constructor(public name: string) {}
-  abstract greeting(this: Person): void;
-}
+let dog: Animal;
+dog = {
+  name: "pochi",
+  crying(cry: string) {
+    console.log(cry);
+  },
+};
 
-class Japanese extends Person {
-  private static instance: Japanese;
+dog.crying("わんわん");
 
-  private constructor(name: string) {
-    super(name);
-  }
+type AnotherAnimal = {
+  name: string;
+  crying(cry: string): void;
+};
 
-  greeting(this: Person): void {
-    console.log(`こんにちは、私は${this.name}です。`);
-  }
-
-  static getInstance() {
-    if (this.instance) {
-      return this.instance;
-    } else {
-      this.instance = new Japanese("Taro");
-      return this.instance;
-    }
-  }
-}
-
-class American extends Person {
-  constructor(name: string) {
-    super(name);
-  }
-
-  greeting(this: Person): void {
-    console.log(`Hi, I'm ${this.name}.`);
-  }
-}
-const taro = Japanese.getInstance();
-
-const john = new American("John");
-john.greeting();
-taro.greeting();
+let cat: AnotherAnimal;
+cat = {
+  name: "tama",
+  crying(cry: string) {
+    console.log(cry);
+  },
+};
+cat.crying("にゃー");
