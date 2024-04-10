71d4fd6805570fd23cc145b4adf7d82fc23bb5b1
course-k <course.k.ky@gmail.com>
2024-04-11 05:00:18 +0900

    function interface

--- src/app.ts
+++ src/app.ts
@@ -1,22 +1,16 @@
-interface Animal extends Play {
-  readonly name: string;
-  crying(cry: string): void;
-}
+type fn = (s: string) => void;
+let greeting: fn;
+greeting = (name: string) => {
+  console.log(`Hi, ${name}`);
+};
+greeting("John");
 
-interface Play {
-  playing(): string;
+interface anotherFn {
+  (s: string): void;
 }
+let anotherGreeting: anotherFn;
+anotherGreeting = (name: string) => {
+  console.log(`Hello, ${name}`);
+};
 
-class Dog implements Animal {
-  constructor(public name: string) {}
-  crying(cry: string): void {
-    console.log(cry);
-  }
-  playing(): string {
-    return "お散歩";
-  }
-}
-
-let pochi: Animal;
-pochi = new Dog("pochi");
-console.log(pochi.playing());
+anotherGreeting("Kevin");
