a5b9b15e1f5d487b8a225666e7df15c8b47ff5f8
course-k <course.k.ky@gmail.com>
2024-04-13 04:00:03 +0900

    practice intersection type

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,56 @@
-interface Animal {
-  readonly name: string;
-  house?: string;
-  crying(cry: string): void;
+type Admin = {
+  name: string;
+  privilege: string[];
+};
+
+type Normal = {
+  name: string;
+  department: string;
+};
+
+type UnionType = Admin & Normal;
+
+let ut: UnionType;
+ut = {
+  name: "Taro",
+  privilege: ["Editor"],
+  department: "Sales",
+};
+
+console.log(ut);
+
+type NumStr = number | string;
+type BoolStr = boolean | string;
+type CombinedType = NumStr & BoolStr;
+
+let ct: CombinedType;
+ct = "hoge";
+console.log(ct);
+
+interface AdminIf {
+  name: string;
+  pirivlege: string[];
 }
 
-class Dog implements Animal {
-  constructor(public name: string) {}
-  crying(cry: string): void {
-    console.log(cry);
-  }
-  playing(): string {
-    return "お散歩";
-  }
+interface NormalIf {
+  name: string;
+  department: string;
 }
+
+type UnionTypeIf = Admin & Normal;
+
+let utIf: UnionTypeIf;
+ut = {
+  name: "Taro",
+  privilege: ["Editor"],
+  department: "Sales",
+};
+
+interface UnionIf extends Admin, Normal {}
+
+let uIf: UnionIf;
+uIf = {
+  name: "hoge",
+  privilege: ["Editor"],
+  department: "Sales",
+};
