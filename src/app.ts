f4e80fa1681c089920bad836c079d7916238b309
course-k <course.k.ky@gmail.com>
2024-04-15 04:04:08 +0900

    practice generics

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,28 @@
-function mergeObj<T, U>(obj1: T, obj2: U) {
+function mergeObj<T extends object, U extends object>(obj1: T, obj2: U) {
   return { ...obj1, ...obj2 };
 }
 
-const mergedObj = mergeObj({ name: "tom" }, { age: 20 });
+const mergedObj = mergeObj({ name: "tom" }, { age: 40 });
 console.log(mergedObj);
+
+interface Lengthy {
+  length: number;
+}
+function countAndDescribe<T extends Lengthy>(element: T): [T, string] {
+  let result = "値がありません。";
+  if (element.length > 0) {
+    result = `値の個数は${element.length}個です。`;
+  }
+  return [element, result];
+}
+
+console.log(countAndDescribe("こんにちは"));
+
+function extractAndConvert<T extends object, U extends keyof T>(
+  obj: T,
+  key: U
+) {
+  return `Value: ${obj[key]}`;
+}
+
+console.log(extractAndConvert({ name: "taro", age: 20 }, "age"));
