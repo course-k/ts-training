e2b4da1033ab6d8d141c05c3b133b1f37473b1ec
course-k <course.k.ky@gmail.com>
2024-04-16 04:21:12 +0900

    first decorator

--- src/app.ts
+++ src/app.ts
@@ -1,28 +1,15 @@
-function mergeObj<T extends object, U extends object>(obj1: T, obj2: U) {
-  return { ...obj1, ...obj2 };
+function Logger(target: Function) {
+  console.log("decorator calling...");
+  console.log(target);
 }
 
-const mergedObj = mergeObj({ name: "tom" }, { age: 40 });
-console.log(mergedObj);
-
-interface Lengthy {
-  length: number;
-}
-function countAndDescribe<T extends Lengthy>(element: T): [T, string] {
-  let result = "値がありません。";
-  if (element.length > 0) {
-    result = `値の個数は${element.length}個です。`;
+@Logger
+class Person {
+  constructor(public name: string = "max") {
+    console.log("Person creating...");
   }
-  return [element, result];
 }
 
-console.log(countAndDescribe("こんにちは"));
+const p = new Person();
 
-function extractAndConvert<T extends object, U extends keyof T>(
-  obj: T,
-  key: U
-) {
-  return `Value: ${obj[key]}`;
-}
-
-console.log(extractAndConvert({ name: "taro", age: 20 }, "age"));
+console.log(p);
