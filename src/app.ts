004d2e4afaa87e0b1c24f5277e08ea0112564f92
course-k <course.k.ky@gmail.com>
2024-04-17 04:35:12 +0900

    practice decolator

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,12 @@
+function Logger(logStr: string) {
+  return function (target: any) {
+    console.log(target);
+    console.log(logStr);
+  };
+}
 function WithTemplate(template: string, hookId: string) {
   return function (target: any) {
+    console.log("テンプレート実行中");
     const element = document.getElementById(hookId);
     const t = new target();
     if (element) {
@@ -8,6 +15,7 @@ function WithTemplate(template: string, hookId: string) {
     }
   };
 }
+@Logger("ロガー実行中")
 @WithTemplate("<h1>Hello world</h1>", "body")
 class Person {
   constructor(public name: string = "max") {
@@ -18,3 +26,56 @@ class Person {
 const p = new Person();
 
 console.log(p);
+
+function PropertyLog(target: any, propertyName: string | Symbol) {
+  console.log("Call Property Decolator");
+  console.log(target);
+  console.log(propertyName);
+}
+
+function MethodLog(
+  target: any,
+  name: string | Symbol,
+  descriptor: PropertyDescriptor
+) {
+  console.log("Call Method Decolator");
+  console.log(target);
+  console.log(name);
+  console.log(descriptor);
+}
+
+function AccesorLog(target: any, name: string, descriptor: PropertyDescriptor) {
+  console.log("Call Accessor Decolator");
+  console.log(target);
+  console.log(name);
+  console.log(descriptor);
+}
+
+function ParamLog(target: any, name: string | Symbol, position: number) {
+  console.log("Call Param Decolator");
+  console.log(target);
+  console.log(name);
+  console.log(position);
+}
+
+class Product {
+  @PropertyLog
+  title: string;
+  private _price: number;
+  @AccesorLog
+  set price(price: number) {
+    if (price > 0) {
+      this._price = price;
+    } else {
+      throw Error("不正な価格です");
+    }
+  }
+  constructor(title: string, price: number) {
+    this.title = title;
+    this._price = price;
+  }
+  @MethodLog
+  getTaxPrice(@ParamLog tax: number) {
+    return this._price * (1 + tax);
+  }
+}
