69b5506ed39d52d62e1a340da26e415d54f25591
course-k <course.k.ky@gmail.com>
2024-04-19 08:36:04 +0900

    practice decorator

--- src/app.ts
+++ src/app.ts
@@ -1,110 +1,52 @@
-// function Logger(logStr: string) {
-//   return function (target: any) {
-//     console.log(target);
-//     console.log(logStr);
-//   };
-// }
-// function WithTemplate(template: string, hookId: string) {
-//   return function <T extends { new (...args: any[]): { name: string } }>(
-//     target: T
-//   ) {
-//     return class extends target {
-//       constructor(..._: any[]) {
-//         super();
-//         console.log("テンプレート実行中");
-//         const element = document.getElementById(hookId);
-//         if (element) {
-//           element.innerHTML = template;
-//           element.querySelector("h1")!.textContent = this.name;
-//         }
-//       }
-//     };
-//   };
-// }
-// @Logger("ロガー実行中")
-// @WithTemplate("<h1>Hello world</h1>", "body")
-// class Person {
-//   constructor(public name: string = "max") {
-//     console.log("Person creating...");
-//   }
-// }
-
-// function PropertyLog(target: any, propertyName: string | Symbol) {
-//   console.log("Call Property Decolator");
-//   console.log(target);
-//   console.log(propertyName);
-// }
-
-// function MethodLog(
-//   target: any,
-//   name: string | Symbol,
-//   descriptor: PropertyDescriptor
-// ) {
-//   console.log("Call Method Decolator");
-//   console.log(target);
-//   console.log(name);
-//   console.log(descriptor);
-// }
-
-// function AccesorLog(target: any, name: string, descriptor: PropertyDescriptor) {
-//   console.log("Call Accessor Decolator");
-//   console.log(target);
-//   console.log(name);
-//   console.log(descriptor);
-// }
-
-// function ParamLog(target: any, name: string | Symbol, position: number) {
-//   console.log("Call Param Decolator");
-//   console.log(target);
-//   console.log(name);
-//   console.log(position);
-// }
-
-// class Product {
-//   @PropertyLog
-//   title: string;
-//   private _price: number;
-//   @AccesorLog
-//   set price(price: number) {
-//     if (price > 0) {
-//       this._price = price;
-//     } else {
-//       throw Error("不正な価格です");
-//     }
-//   }
-//   constructor(title: string, price: number) {
-//     this.title = title;
-//     this._price = price;
-//   }
-//   @MethodLog
-//   getTaxPrice(@ParamLog tax: number) {
-//     return this._price * (1 + tax);
-//   }
-// }
-
-function AutoBind(
-  _target: any,
-  _propName: string,
-  descriptor: PropertyDescriptor
-) {
-  const orgMethod = descriptor.value;
-  const adjDescriptor: PropertyDescriptor = {
-    get() {
-      const boundFn = orgMethod.bind(this);
-      return boundFn;
-    },
+interface ValidatorConfig {
+  [prop: string]: {
+    [validatableProp: string]: string[];
   };
+}
+const registeredValidators: ValidatorConfig = {};
 
-  return adjDescriptor;
+function Required(target: any, name: string) {
+  registeredValidators[target.constructor.name] = {
+    [name]: ["required"],
+  };
 }
 
-class Msg {
-  message = "clicked";
-  @AutoBind
-  showMessage() {
-    console.log(this.message);
+function PositiveNumber(target: any, name: string) {
+  registeredValidators[target.constructor.name] = {
+    [name]: ["positive"],
+  };
+}
+
+function Validate(obj:any) {
+  const objValidatorConfig = registeredValidators[obj.constructor.name];
+  if(!objValidatorConfig){
+    return true
+  }
+
+  for (const validator in Object.values(objValidatorConfig) ){
+    switch(validator) {
+      case
+    }
   }
 }
-const m = new Msg();
-const myButton = document.querySelector("#myButton")!;
-myButton.addEventListener("click", m.showMessage);
+
+class Course {
+  @Required
+  title: string;
+  @PositiveNumber
+  @Required
+  price: number;
+  constructor(title: string, price: number) {
+    this.title = title;
+    this.price = price;
+  }
+}
+
+const form = document.querySelector("form")!;
+form.addEventListener("submit", (ev) => {
+  ev.preventDefault();
+  const titleEl = form.querySelector("#title") as HTMLInputElement;
+  const priceEl = form.querySelector("#price") as HTMLInputElement;
+  const course = new Course(titleEl.value, +priceEl.value);
+  console.log(course);
+});
